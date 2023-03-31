2769145800cd67ec80be4062d45053f3a3372623
maximiloi <maximiloi@gmail.com>
2023-03-31 13:49:43 +0300

    feat: add mobile menu

--- shelter/src/js/files/script.js
+++ shelter/src/js/files/script.js
@@ -9,17 +9,17 @@ const documentActions = e => {
 
     // открываем меню и закрываем меню
     if (targetElem.closest('.nav__burger')) {
-        document.documentElement.classList.toggle('_open');
+        document.documentElement.classList.toggle('_open-nav');
     }
 
     // Клик по выбранной ссылке
     if (targetElem.closest('.nav__link')) {
-        document.documentElement.classList.remove('_open');
+        document.documentElement.classList.remove('_open-nav');
     }
 
     // Если клик на любом другом поле
     if (!targetElem.closest('.nav__burger') && !targetElem.closest('.nav__list')) {
-        document.documentElement.classList.remove('_open');
+        document.documentElement.classList.remove('_open-nav');
     }
 };
 
