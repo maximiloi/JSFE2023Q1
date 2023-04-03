62cbf2638b5514a2d040d00fa333c4a27bcb8821
maximiloi <maximiloi@gmail.com>
2023-04-03 22:50:56 +0300

    feat: add generate modal and card with pets

--- shelter/src/js/files/script.js
+++ shelter/src/js/files/script.js
@@ -1,25 +1,45 @@
 console.log('self-check: 100 point');
-
 console.log('');
 
 const documentActions = e => {
     const targetElem = e.target;
-    // console.log('%c [ targetElem ]-29', 'font-size:13px; background:pink; color:#bf2c9f;', targetElem);
+    const modalElements = document.querySelectorAll('.modal-pets__wrapper');
+    // console.log('targetElem: ', targetElem);
+
     // Работа с меню
 
     // открываем меню и закрываем меню
     if (targetElem.closest('.nav__burger')) {
+        document.body.classList.toggle('_layout');
         document.documentElement.classList.toggle('_open-nav');
     }
 
-    // Клик по выбранной ссылке
-    if (targetElem.closest('.nav__link')) {
+    // Клик по выбранной ссылке или любом другом месте
+    if (
+        targetElem.closest('.nav__link')
+        // || (!targetElem.closest('.nav__burger') && !targetElem.closest('.nav__link'))
+    ) {
+        document.body.classList.remove('_layout');
         document.documentElement.classList.remove('_open-nav');
+
+        modalElements.forEach(element => {
+            element.classList.remove('_active');
+        });
     }
 
-    // Если клик на любом другом поле
-    if (!targetElem.closest('.nav__burger') && !targetElem.closest('.nav__list')) {
-        document.documentElement.classList.remove('_open-nav');
+    if (targetElem.closest('.slider__button')) {
+        // const modalElements = document.querySelectorAll('.modal-pets__wrapper');
+
+        modalElements.forEach(element => {
+            if (element.dataset.petsName === targetElem.dataset.petsName) {
+                document.body.classList.toggle('_layout');
+                element.classList.add('_active');
+            }
+        });
+    }
+
+    if (targetElem.closest('.modal-pets__button')) {
+        targetElem.parentElement.parentElement.classList.remove('_active');
     }
 };
 
