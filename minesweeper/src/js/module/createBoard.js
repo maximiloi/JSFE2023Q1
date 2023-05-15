94f86289c3d0ae6f9f094d7a952dfd5139745e3a
maximiloi <maximiloi@gmail.com>
2023-05-15 14:53:07 +0300

    feat: add createPage function

---
+++ minesweeper/src/js/module/createBoard.js
@@ -0,0 +1,15 @@
+import createElement from '../utils/createElement.js';
+
+function createBoard(widthBoard) {
+  const boardWrapper = document.querySelector('.board');
+
+  for (let i = 0; i < widthBoard * widthBoard; i += 1) {
+    const square = createElement('div', `square border`);
+    square.setAttribute('id', i);
+    boardWrapper.style.width = `${40 * widthBoard}px`;
+    boardWrapper.style.height = `${40 * widthBoard}px`;
+    boardWrapper.appendChild(square);
+  }
+}
+
+export default createBoard;
