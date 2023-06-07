da587158baa9133d6c2fdd84f49f4d9b30060c85
maximiloi <maximiloi@gmail.com>
2023-06-07 12:48:36 +0300

    init: add ApiKey to app

--- news-js/src/components/controller/appLoader.js
+++ news-js/src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
-import Loader from './loader';
+import Loader from "./loader";
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super("https://newsapi.org/v2/", {
+      apiKey: "97a2f1568e1746d293951df10676b696", // получите свой ключ https://newsapi.org/
+    });
+  }
 }
 
 export default AppLoader;
