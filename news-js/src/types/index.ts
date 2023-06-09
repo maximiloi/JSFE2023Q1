0fe77ed91761520ab6e925ddd8667ca35e1916eb
maximiloi <maximiloi@gmail.com>
2023-06-09 13:03:31 +0300

    feat: add interface UNews to types

--- news-js/src/types/index.ts
+++ news-js/src/types/index.ts
@@ -6,3 +6,18 @@ export interface UEndpoint {
   endpoint: string;
   options: UOption;
 }
+
+export interface UResponse {
+  status: string;
+  sources: UNews[];
+}
+
+type UNews = {
+  id: string,
+  name: string,
+  description: string,
+  url: string,
+  category: string,
+  language: string,
+  country: string
+};
