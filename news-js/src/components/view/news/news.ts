6eb89eb94edc6c169127fd7551727355603ef831
maximiloi <maximiloi@gmail.com>
2023-06-12 20:18:49 +0300

    feat: add img no news

--- news-js/src/components/view/news/news.ts
+++ news-js/src/components/view/news/news.ts
@@ -19,7 +19,7 @@ class News {
 
       const metaPhotoEl: Element | null = newsClone.querySelector('.news__meta-photo');
       if (metaPhotoEl instanceof HTMLElement) {
-        metaPhotoEl.style.backgroundImage = `url(${item.urlToImage || '../../../assets/news_placeholder.jpg'})`;
+        metaPhotoEl.style.backgroundImage = `url(${item.urlToImage || 'https://clipground.com/images/no-image-png-5.jpg'})`;
       }
 
       const metaAuthorEl: Element | null = newsClone.querySelector('.news__meta-author');
