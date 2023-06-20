89a1c2aa222d27b79f3ca90e8bffb08ac8449c77
maximiloi <maximiloi@gmail.com>
2023-06-20 21:56:31 +0300

    feat: add link footer

--- rs-css/src/components/Footer/index.ts
+++ rs-css/src/components/Footer/index.ts
@@ -1,23 +1,26 @@
-import { UElements } from '../../types/types';
 import createElement from '../../utils/createElement';
 import './index.scss';
 
 import rssLogo from '../../assets/rsschool_logo.png'
 import githubLogo from '../../assets/github_logo.png'
 
-const footerSection: UElements[] = [
-  { tagName: 'section', className: 'app__footer footer' },
-  { tagName: 'img', className: 'footer__img footer__rsschool' },
-  { tagName: 'p', className: 'footer__text', textContent: '2023 - RSSchool' },
-  { tagName: 'img', className: 'footer__img footer__github' },
-]
-
 function createFooter(parentClassName: string): void {
-  const footerElem: HTMLElement = createElement(footerSection[0].tagName, footerSection[0].className);
-  const footerImgRSSchool: HTMLImageElement = createElement(footerSection[1].tagName, footerSection[1].className) as HTMLImageElement;
+  const footerElem: HTMLElement = createElement('section', 'app__footer footer');
+
+  const footerRSSLinkElem: HTMLAnchorElement = createElement('a', 'footer__link') as HTMLAnchorElement;
+  footerRSSLinkElem.href = 'https://rs.school/js/' as string;
+  footerRSSLinkElem.target = '_blank' as string;
+
+  const footerImgRSSchool: HTMLImageElement = createElement('img', 'footer__img footer__rsschool') as HTMLImageElement;
   footerImgRSSchool.src = rssLogo as string;
-  const footerText: HTMLElement = createElement(footerSection[2].tagName, footerSection[2].className, footerSection[2].textContent);
-  const footerImgGithub: HTMLImageElement = createElement(footerSection[3].tagName, footerSection[3].className) as HTMLImageElement;
+
+  const footerText: HTMLElement = createElement('p', 'footer__text', '2023 - RSSchool');
+
+  const footerGithubLinkElem: HTMLAnchorElement = createElement('a', 'footer__link') as HTMLAnchorElement;
+  footerGithubLinkElem.href = 'https://github.com/maximiloi' as string;
+  footerGithubLinkElem.target = '_blank' as string;
+
+  const footerImgGithub: HTMLImageElement = createElement('img', 'footer__img footer__github') as HTMLImageElement;
   footerImgGithub.src = githubLogo as string;
 
   const parentElement: HTMLElement | null = document.querySelector(parentClassName);
@@ -25,7 +28,9 @@ function createFooter(parentClassName: string): void {
     parentElement.append(footerElem);
   };
 
-  footerElem.append(footerImgRSSchool, footerText, footerImgGithub)
+  footerRSSLinkElem.append(footerImgRSSchool);
+  footerGithubLinkElem.append(footerImgGithub)
+  footerElem.append(footerRSSLinkElem, footerText, footerGithubLinkElem)
 }
 
 export default createFooter;
