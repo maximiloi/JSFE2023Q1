fbee78e93584a00ddf2994da6c0290a9550b8a1c
maximiloi <maximiloi@gmail.com>
2023-06-21 10:36:08 +0300

    refactor: rewritten the addition of the app wrap

--- rs-css/src/components/Header/index.ts
+++ rs-css/src/components/Header/index.ts
@@ -4,7 +4,6 @@ import './index.scss';
 import logoImg from '../../public/favicon.png'
 
 function createHeader(parentClassName: string): void {
-  const appElem: HTMLElement = createElement('div', 'app');
   const headerElem: HTMLElement = createElement('section', 'app__header header');
   const headerLogoElem: HTMLImageElement = createElement('img', 'header__logo') as HTMLImageElement;
   headerLogoElem.alt = 'logo images';
@@ -12,11 +11,10 @@ function createHeader(parentClassName: string): void {
   const headerTitleElem: HTMLElement = createElement('h1', 'header__title', 'RSSchool | Css Dinner | iloi');
 
   headerElem.append(headerLogoElem, headerTitleElem);
-  appElem.append(headerElem);
 
   const parentElement: HTMLElement | null = document.querySelector(parentClassName);
   if (parentElement) {
-    parentElement.append(appElem);
+    parentElement.append(headerElem);
   };
 }
 
--- rs-css/src/index.ts
+++ rs-css/src/index.ts
@@ -1,8 +1,16 @@
+import createElement from './utils/createElement';
 import createHeader from './components/Header/index';
 import createMain from './components/Main';
 import createFooter from './components/Footer/index';
 import './styles.scss';
 
-createHeader('body');
+const appElem: HTMLElement = createElement('div', 'app');
+
+const parentElement: HTMLElement | null = document.querySelector('body');
+if (parentElement) {
+  parentElement.append(appElem);
+};
+
+createHeader('.app');
 createMain('.app');
 createFooter('.app');
