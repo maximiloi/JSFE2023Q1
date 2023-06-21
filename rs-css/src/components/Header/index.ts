62c416ef1b5b192ade2aee432a4fb5c49acad4e7
maximiloi <maximiloi@gmail.com>
2023-06-21 10:25:01 +0300

    refactor: Header create element

--- rs-css/src/components/Header/index.ts
+++ rs-css/src/components/Header/index.ts
@@ -1,23 +1,15 @@
-import { UElements } from '../../types/types';
 import createElement from '../../utils/createElement';
 import './index.scss';
 
 import logoImg from '../../public/favicon.png'
 
-const headerSection: UElements[] = [
-  { tagName: 'div', className: 'app' },
-  { tagName: 'section', className: 'app__header header' },
-  { tagName: 'img', className: 'header__logo' },
-  { tagName: 'h1', className: 'header__title', textContent: 'RSSchool | Css Dinner | iloi' }
-]
-
 function createHeader(parentClassName: string): void {
-  const appElem: HTMLElement = createElement(headerSection[0].tagName, headerSection[0].className);
-  const headerElem: HTMLElement = createElement(headerSection[1].tagName, headerSection[1].className);
-  const headerLogoElem: HTMLImageElement = createElement(headerSection[2].tagName, headerSection[2].className) as HTMLImageElement;
+  const appElem: HTMLElement = createElement('div', 'app');
+  const headerElem: HTMLElement = createElement('section', 'app__header header');
+  const headerLogoElem: HTMLImageElement = createElement('img', 'header__logo') as HTMLImageElement;
   headerLogoElem.alt = 'logo images';
   headerLogoElem.src = logoImg as string;
-  const headerTitleElem: HTMLElement = createElement(headerSection[3].tagName, headerSection[3].className, headerSection[3].textContent);
+  const headerTitleElem: HTMLElement = createElement('h1', 'header__title', 'RSSchool | Css Dinner | iloi');
 
   headerElem.append(headerLogoElem, headerTitleElem);
   appElem.append(headerElem);
