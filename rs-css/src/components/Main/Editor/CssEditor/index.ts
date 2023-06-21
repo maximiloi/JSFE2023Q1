06ff8f23af4c4164c0be1f80a2614085c1b3d096
maximiloi <maximiloi@gmail.com>
2023-06-21 16:24:57 +0300

    feat: add css editor to editor

---
+++ rs-css/src/components/Main/Editor/CssEditor/index.ts
@@ -0,0 +1,46 @@
+import addToParentElement from "../../../../utils/addToParentElement";
+import createElement from "../../../../utils/createElement";
+
+import './index.scss';
+
+function createCssEditor(parentClassName: string): void {
+  const editorCssElem: HTMLElement = createElement('div', 'editor-pane');
+  addToParentElement(parentClassName, editorCssElem);
+
+  const editorCssTitleElem: HTMLElement = createElement('div', 'editor-pane__title');
+  addToParentElement('.editor-pane', editorCssTitleElem);
+
+  const editorNameElem: HTMLElement = createElement('h4', 'editor-pane__file-name');
+  editorNameElem.textContent = 'CSS Editor'
+  addToParentElement('.editor-pane__title', editorNameElem);
+
+  const editorCssFileNameElem: HTMLElement = createElement('span', 'editor-pane__file-name');
+  editorCssFileNameElem.textContent = 'style.css'
+  addToParentElement('.editor-pane__title', editorCssFileNameElem);
+
+  const editorCssFileElem: HTMLElement = createElement('div', 'editor-pane__file css-view');
+  addToParentElement('.editor-pane', editorCssFileElem);
+
+  const editorCssNumElem: HTMLElement = createElement('div', 'editor-pane__number');
+  addToParentElement('.editor-pane__file', editorCssNumElem);
+  let numberEditor = '';
+  for (let i = 1; i <= 20; i += 1) {
+    numberEditor += `${i}<br>`;
+  }
+  editorCssNumElem.innerHTML = numberEditor;
+
+  const editorCssInputElem: HTMLElement = createElement('div', 'editor-pane__input');
+  addToParentElement('.editor-pane__file', editorCssInputElem);
+
+  const editorCssInputFieldElem: HTMLInputElement = createElement('input', 'editor-pane__input--answer input-answer') as HTMLInputElement;
+  editorCssInputFieldElem.type = 'text' as string;
+  editorCssInputFieldElem.placeholder = 'Type in a CSS selector' as string;
+  addToParentElement('.editor-pane__input', editorCssInputFieldElem);
+
+  const editorCssInpuTextElem: HTMLElement = createElement('div', 'editor-pane__input--text');
+  editorCssInpuTextElem.innerHTML = `{<br>/* Styles would go here. */<br>}<br><br>
+  /*<br>Type a number to skip to a level.<br>Ex → "5" for level 5<br>*/`;
+  addToParentElement('.editor-pane__input', editorCssInpuTextElem);
+}
+
+export default createCssEditor;
