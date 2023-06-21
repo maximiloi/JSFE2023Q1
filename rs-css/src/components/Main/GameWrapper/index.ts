054b8ea573a0fe3529225e9f23895974e8183874
maximiloi <maximiloi@gmail.com>
2023-06-21 16:02:46 +0300

    feat: add style GameWrapper

--- rs-css/src/components/Main/GameWrapper/index.ts
+++ rs-css/src/components/Main/GameWrapper/index.ts
@@ -1,6 +1,8 @@
 import createElement from "../../../utils/createElement";
 import addToParentElement from "../../../utils/addToParentElement";
 
+import './index.scss';
+
 function createGameWrapper(parentClassName: string): void {
   const gameWrapperElem: HTMLElement = createElement('div', 'main__game game__wrapper');
 
