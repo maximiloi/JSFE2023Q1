f36f2fc07365fbd1874d8a9f799345009792fad4
maximiloi <maximiloi@gmail.com>
2023-06-23 11:35:08 +0300

    feat: add game table and level

--- rs-css/src/components/Main/GameWrapper/index.ts
+++ rs-css/src/components/Main/GameWrapper/index.ts
@@ -4,9 +4,17 @@ import addToParentElement from "../../../utils/addToParentElement";
 import './index.scss';
 
 function createGameWrapper(parentClassName: string): void {
-  const gameWrapperElem: HTMLElement = createElement('div', 'main__game game__wrapper');
-
+  const gameWrapperElem: HTMLElement = createElement('div', 'main__game game');
   addToParentElement(parentClassName, gameWrapperElem);
+
+  const gameTableElem: HTMLElement = createElement('div', 'game__table');
+  addToParentElement('.game', gameTableElem);
+
+  const gameLevelElem: HTMLElement = createElement('div', 'game__level level');
+  addToParentElement('.game', gameLevelElem);
+  const gameLevelTitleElem: HTMLElement = createElement('h3', 'level__title', 'Level');
+  addToParentElement('.level', gameLevelTitleElem);
+
 }
 
 export default createGameWrapper;
