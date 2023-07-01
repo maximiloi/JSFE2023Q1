2ca6f8780432b2243c20675db39af232b2008809
maximiloi <maximiloi@gmail.com>
2023-07-01 23:41:26 +0300

    feat: add table on game field

--- rs-css/src/components/Main/GameWrapper/index.ts
+++ rs-css/src/components/Main/GameWrapper/index.ts
@@ -5,13 +5,28 @@ import { LEVELS } from "../../Levels/Levels";
 import './index.scss';
 
 function createGameWrapper(parentClassName: string): void {
-  const gameWrapperElem: HTMLElement = createElement('div', 'main__game game');
-  addToParentElement(parentClassName, gameWrapperElem);
+  const gameElem: HTMLElement = createElement('div', 'main__game game');
+  addToParentElement(parentClassName, gameElem);
 
-  const gameTableElem: HTMLElement = createElement('div', 'game__table');
-  addToParentElement('.game', gameTableElem);
+  const gameWrapperElem: HTMLElement = createElement('div', 'game__wrapper');
+  addToParentElement('.game', gameWrapperElem);
   const gameTableTitleElem: HTMLElement = createElement('h3', 'game__title');
-  addToParentElement('.game__table', gameTableTitleElem);
+  addToParentElement('.game__wrapper', gameTableTitleElem);
+  const gameTableElem: HTMLElement = createElement('div', 'game__tablet table');
+  addToParentElement('.game__wrapper', gameTableElem);
+
+  const TableWrapperElem: HTMLElement = createElement('div', 'table__wrapper');
+  addToParentElement('.table', TableWrapperElem);
+  const TableSurfaceElem: HTMLElement = createElement('div', 'table__surface');
+  addToParentElement('.table__wrapper', TableSurfaceElem);
+  const TableAreaElem: HTMLElement = createElement('div', 'table__area');
+  addToParentElement('.table__wrapper', TableAreaElem);
+  const TablEdgeElem: HTMLElement = createElement('div', 'table__edge');
+  addToParentElement('.table', TablEdgeElem);
+  const TablLegRElem: HTMLElement = createElement('div', 'table__leg table__leg--right');
+  addToParentElement('.table__edge', TablLegRElem);
+  const TablLegLElem: HTMLElement = createElement('div', 'table__leg table__leg--left');
+  addToParentElement('.table__edge', TablLegLElem);
 
   const gameLevelElem: HTMLElement = createElement('div', 'game__level level');
   addToParentElement('.game', gameLevelElem);
