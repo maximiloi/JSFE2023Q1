588a403d2d049b408d6b80b9d12b92222a0c4078
maximiloi <maximiloi@gmail.com>
2023-07-03 15:58:13 +0300

    feat: add answer check

---
+++ rs-css/src/components/checkInput/index.ts
@@ -0,0 +1,51 @@
+import { LEVELS } from "../Levels/Levels";
+import renderLevel from "../renderLevel";
+
+function checkWin(inputValue: string): void {
+  const currentLevel: string | null = localStorage.getItem('current-level');
+  if (!currentLevel) return;
+  const levelNumber: number = parseInt(currentLevel, 10);
+
+  if (inputValue === LEVELS[levelNumber].selector) {
+    const inputAnswer: HTMLInputElement | null = document.querySelector('.input-answer');
+    if (!inputAnswer) return;
+    inputAnswer.value = '';
+
+    const levelCheckmark: NodeListOf<Element> = document.querySelectorAll('.level__checkmark');
+    levelCheckmark[levelNumber].classList.add('check');
+
+    const completedLevels: number[] = JSON.parse(localStorage.getItem('completed-levels') || '[]') as number[];
+    if (!completedLevels.includes(levelNumber)) {
+      completedLevels.push(levelNumber);
+      localStorage.setItem('completed-levels', JSON.stringify(completedLevels));
+    }
+
+    const levelNextNumber: number = levelNumber + 1;
+    const levelNext: string = levelNextNumber.toString();
+    localStorage.setItem('current-level', levelNext);
+    renderLevel(levelNext);
+  }
+}
+
+function checkInput(): void {
+  const inputAnswer: HTMLInputElement | null = document.querySelector('.input-answer');
+  if (!inputAnswer) return;
+
+  inputAnswer.addEventListener("keydown", (event: Event): void => {
+    const keyboardEvent = event as KeyboardEvent;
+    if (keyboardEvent.key === "Enter") {
+      const inputValue: string = inputAnswer.value
+      checkWin(inputValue)
+    }
+  })
+
+  const inpurButton: Element | null = document.querySelector('.editor-pane__button');
+  if (!inpurButton) return;
+
+  inpurButton.addEventListener("click", (): void => {
+    const inputValue: string = inputAnswer.value
+    checkWin(inputValue)
+  })
+}
+
+export default checkInput;
--- rs-css/src/index.ts
+++ rs-css/src/index.ts
@@ -2,11 +2,12 @@ import './styles.scss';
 
 import createElement from './utils/createElement';
 import addToParentElement from './utils/addToParentElement';
-import clickHandler from './utils/clickHandler';
+import clickLevel from './utils/clickLevel';
 import createHeader from './components/Header/index';
 import createMain from './components/Main';
 import createFooter from './components/Footer/index';
 import checkLocalStorage from './components/localStorage';
+import checkInput from './components/checkInput';
 
 const appElem: HTMLElement = createElement('div', 'app');
 addToParentElement('body', appElem);
@@ -17,4 +18,5 @@ createFooter('.app');
 
 checkLocalStorage();
 
-clickHandler();
+clickLevel();
+checkInput();
