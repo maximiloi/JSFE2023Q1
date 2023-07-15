becc8d4ff76f8821230b9030a5218c35d45e290d
maximiloi <maximiloi@gmail.com>
2023-07-15 21:03:33 +0300

    feat: add winners page

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -2,6 +2,9 @@ import './css/normalize.scss';
 import './style.scss';
 import creatAddElementToPage from './utils/createElement';
 import createHeader from './userInterface/header/index';
+import renderWinners from './userInterface/winners/index';
 
 creatAddElementToPage('body', 'div', 'app');
 createHeader();
+creatAddElementToPage('.app', 'main', 'main');
+renderWinners();
