5fe2946f004bc6bb9fcafe5ffec7339578cf70d0
maximiloi <maximiloi@gmail.com>
2023-07-16 15:50:39 +0300

    merge

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -2,6 +2,11 @@ import './css/normalize.scss';
 import './style.scss';
 import creatAddElementToPage from './utils/createElement';
 import createHeader from './userInterface/header/index';
+import renderFooter from "./userInterface/footer";
+import renderInputGroup from './userInterface/main/inputGroup';
 
 creatAddElementToPage('body', 'div', 'app');
 createHeader();
+creatAddElementToPage('.app', 'main', 'app__main main');
+renderInputGroup();
+renderFooter();
