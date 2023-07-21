5a503a2282c1ff16d5d71cd6359b96f319e08ec6
maximiloi <maximiloi@gmail.com>
2023-07-21 09:57:36 +0300

    feat: added functions get, numberCarsInGarage, getGarage, getCar, CreateCar

--- async-race/src/api/apiGarage.ts
+++ async-race/src/api/apiGarage.ts
@@ -1,7 +1,8 @@
-import { CarWithId } from "./apiType";
+import checkLocalStorage from "../utils/checkLocalStorage";
+import { Car, CarWithId } from "./apiType";
 
 const API_URL = 'http://127.0.0.1:3000';
-const NUMBER_CARS_ON_PAGE = 7;
+export const NUMBER_CARS_ON_PAGE = 7;
 const QUERY_URL = {
   garage: '/garage',
   engine: '/engine',
@@ -17,17 +18,70 @@ function generateQueryString(queryParams: QueryParams[] = []): string {
   return queryParams.length ? `?${queryParams.map(x => `${x.key}=${x.value}`).join('&')}` : '';
 }
 
-async function getGarage(): Promise<CarWithId[] | null> {
+export async function numberCarsInGarage(): Promise<number> {
+  const numberPage: number = Number(checkLocalStorage());
+
   try {
-    const queryParams: QueryParams[] = [{ key: '_page', value: 1 }, { key: '_limit', value: NUMBER_CARS_ON_PAGE }]
-    const response: Response = await fetch(`${API_URL}${QUERY_URL.garage}${generateQueryString(queryParams)}`);
-    console.log('response: ', response);
+    const queryParams: QueryParams[] = [{ key: '_page', value: numberPage }, { key: '_limit', value: NUMBER_CARS_ON_PAGE }]
+    const response: Response = await fetch(`${API_URL}${QUERY_URL.garage}${generateQueryString(queryParams)}`, {
+      method: 'GET'
+    });
+    const numberCars: number = Number(response.headers.get('X-Total-Count'));
+    return numberCars;
+  } catch (error) {
+    console.error('error: ', error);
+    throw error;
+  }
+}
+
+export async function getGarage(): Promise<CarWithId[]> {
+  const numberPage: number = Number(checkLocalStorage());
+
+  try {
+    const queryParams: QueryParams[] = [{ key: '_page', value: numberPage }, { key: '_limit', value: NUMBER_CARS_ON_PAGE }]
+    const response: Response = await fetch(`${API_URL}${QUERY_URL.garage}${generateQueryString(queryParams)}`, {
+      method: 'GET'
+    });
     const data = await response.json() as CarWithId[];
     return data;
   } catch (error) {
     console.error('error: ', error);
-    return null;
+    throw error;
   }
 }
 
-export default getGarage;
+export async function getCar(id?: number): Promise<CarWithId> {
+  try {
+    const response: Response = await fetch(`${API_URL}${QUERY_URL.garage}/${id}`, {
+      method: 'GET'
+    });
+    const data = await response.json() as CarWithId;
+    console.log('getCar data: ', data);
+    return data;
+  } catch (error) {
+    console.error('error: ', error);
+    throw error;
+  }
+}
+
+await getCar(1);
+
+export async function CreateCar(data: Car): Promise<Car> {
+  try {
+    const response: Response = await fetch(`${API_URL}${QUERY_URL.garage}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const newCar = await response.json() as CarWithId;
+    console.log('newCar: ', newCar);
+    return newCar;
+  } catch (error) {
+    console.error('error: ', error);
+    throw error;
+  }
+}
+
+// await CreateCar({ name: 'newCar', color: '#ffffff' });
