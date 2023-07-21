de5698d290c073be18fc39d134d32ed82bf841ee
maximiloi <maximiloi@gmail.com>
2023-07-21 09:46:09 +0300

    refactor: rename file

--- async-race/src/userInterface/main/renderGarage.ts
+++ async-race/src/userInterface/main/renderGarage.ts
@@ -4,28 +4,26 @@ import { CarWithId } from "../../api/apiType";
 import { getGarage, numberCarsInGarage } from "../../api/apiGarage";
 import renderCar from "./renderCar";
 
-const apiData: CarWithId[] = await getGarage();
-const carNumbers: number = await numberCarsInGarage();
-const pageNumbers: string = checkLocalStorage();
 
 async function renderGarage(): Promise<void> {
+  const apiData: CarWithId[] = await getGarage();
+  const carNumbers: number = await numberCarsInGarage();
+  const pageNumbers: string = checkLocalStorage();
+
   creatAddElementToPage('.main', 'section', 'main__garage garage');
   creatAddElementToPage('.garage', 'h2', 'garage__title', 'Garage');
   creatAddElementToPage('.garage__title', 'span', 'garage__cars-number');
-  const carNumbersEl: HTMLElement | null = document.querySelector('.garage__cars-number');
-  if (!carNumbersEl) return;
+  const carNumbersEl = document.querySelector('.garage__cars-number') as HTMLElement;
   carNumbersEl.innerHTML = `(${carNumbers})`;
   creatAddElementToPage('.garage', 'div', 'garage__wrapper');
   creatAddElementToPage('.garage__wrapper', 'h3', 'garage__title garage__title--page', 'Page #');
   creatAddElementToPage('.garage__title--page', 'span', 'garage__page-number');
-  const pageNumbersEl: HTMLElement | null = document.querySelector('.garage__page-number');
-  if (!pageNumbersEl) return;
-  pageNumbersEl.innerHTML = `${pageNumbers}`;
+  const pageNumbersEl = document.querySelector('.garage__page-number') as HTMLElement;
+  pageNumbersEl.innerHTML = ` ${pageNumbers}`;
   creatAddElementToPage('.garage__wrapper', 'div', 'garage__wrapper--button');
   creatAddElementToPage('.garage__wrapper--button', 'button', 'garage__button garage__button--prev', 'prev');
   creatAddElementToPage('.garage__wrapper--button', 'button', 'garage__button garage__button--next', 'next');
   creatAddElementToPage('.garage', 'div', 'garage__cars cars__list');
-  if (!apiData) return;
   await renderCar(apiData);
 }
 
