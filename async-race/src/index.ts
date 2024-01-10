a86c5f2d184b6ac94265b8849eb64012b69805c1
maximiloi <maximiloi@gmail.com>
2024-01-10 12:05:10 +0300

    refactor: rename file

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,7 +1,7 @@
 import './css/normalize.scss';
 import './style.scss';
 import creatAddElementToPage from './utils/createElement';
-import { documentActions } from './utils/queryClick';
+import { documentActions } from './utils/documentActions';
 import createHeader from './userInterface/header';
 import renderInputGroup from './userInterface/main/inputGroup';
 import renderGarage from './userInterface/main/renderGarage';
